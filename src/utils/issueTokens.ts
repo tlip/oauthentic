77b89d69152aaa89fddbb69c8e699d4b8f38bc90
Thomas Lipari <thomas.lipari@gmail.com>
2019-05-10 12:00:23 -0700

    refactor issueTokens

--- src/utils/issueTokens.ts
+++ src/utils/issueTokens.ts
@@ -1,36 +1,28 @@
 import crypto from 'crypto';
 import signale from 'signale';
 
-import { User } from '../db/entity/User.entity';
-import { AccessToken } from '../db/entity/AccessToken.entity';
-import { RefreshToken } from '../db/entity/RefreshToken.entity';
 import db from '../db';
 
 const issueTokens = async (userId: string | null, clientId: string, done: Function) => {
-
-  let accessToken: AccessToken | undefined;
-  let refreshToken: RefreshToken | undefined;
 
   const atoken = crypto.randomBytes(128).toString('hex');
   const rToken = crypto.randomBytes(128).toString('hex');
 
   try {
-    accessToken = await db.AccessTokens.save(atoken, userId, clientId);
-    if (!accessToken) { return done(null, false); }
+    await db.AccessTokens.save(atoken, userId, clientId);
   } catch (error) {
     signale.error('[IssueTokensError :: issueTokens :: accessToken]');
     throw error;
   }
   
   try {
-    refreshToken = await db.RefreshTokens.save(rToken, userId, clientId);
-    if (!refreshToken) { return done(null, false); }
+    await db.RefreshTokens.save(rToken, userId, clientId);
   } catch (error) {
     signale.error('[IssueTokensError :: issueTokens :: refeshToken]');
     throw error;
   }
 
-  return done(null, atoken, rToken);
+  return done(null, atoken, rToken, { expires: 3600 });
 
 };
 
